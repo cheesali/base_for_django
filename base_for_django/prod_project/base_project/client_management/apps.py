9c9427cdf680011ec1ef39c74bec2726c2f1ced5
Alina <70215718+cheesali@users.noreply.github.com>
2024-06-26 14:54:51 +0200

    Delete base_for_django/prod_project/base_project directory

--- base_for_django/prod_project/base_project/client_management/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClientManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'client_management'
--- base_for_django/prod_project/base_project/client_management/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.db.utils import OperationalError
-from django.db import connections
-
-def index(request):
-    return render(request, 'index.html')
